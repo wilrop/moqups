7b51ffbdf97a35a5cae9b2ccbfa39d42cbd8a21c
Willem Röpke <willem.ropke@vub.be>
2021-10-22 11:04:11 +0200

    Skeletonn for iterated best response and fictitious play

---
+++ IBR.py
@@ -0,0 +1,9 @@
+from best_response import best_response
+
+
+def iterated_best_response():
+    return 0
+
+
+if __name__ == '__main__':
+    iterated_best_response()
---
+++ fictitious_play.py
@@ -0,0 +1,6 @@
+def fictitious_play():
+    return 0
+
+
+if __name__ == '__main__':
+    fictitious_play()
