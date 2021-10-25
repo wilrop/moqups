fc099a3a5b21272a874eedf59f6d9032dad0cf31
Willem Röpke <willem.ropke@vub.be>
2021-10-25 17:43:59 +0200

    Create util file to hold shared functions for example used to pretty print

---
+++ util.py
@@ -0,0 +1,29 @@
+def print_psne(psne_lst):
+    """
+    This function will pretty print the list of PSNE.
+    :param psne_lst: A list of PSNE.
+    :return: /
+    """
+    print('There are a total of ' + repr(len(psne_lst)) + ' pure strategy Nash equilibria')
+    for psne in psne_lst:
+        print(repr(psne))
+
+
+def print_ne(ne):
+    """
+    This function will pretty print a Nash equilibrium
+    :param ne: The joint strategy.
+    :return: /
+    """
+    print('The Nash equilibrium that was found is the joint strategy ' + repr(ne))
+
+
+def print_all_ne(ne_lst):
+    """
+    This function will pretty print the list of Nash equilibria.
+    :param ne_lst: A list of Nash equilibria.
+    :return: /
+    """
+    print('There are a total of ' + repr(len(ne_lst)) + ' Nash equilibria')
+    for ne in ne_lst:
+        print(repr(ne))
