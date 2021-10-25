2be6521b3c0cc8cbd7db6d56b730992d58a09489
Willem Röpke <willem.ropke@vub.be>
2021-10-25 08:21:28 +0200

    Implement best response optimisation

--- best_response.py
+++ best_response.py
@@ -1,47 +1,28 @@
 import games
 import numpy as np
-import scipy.optimize as opt
-
-'''
-def calc_expected_vec(policy, returns):
-    """
-    This method calculates the expected vector for a policy and matrix of reward vectors.
-    :param policy: The policy.
-    :param returns: The matrix of reward vectors.
-    :return: The expected vector when applying this policy on the reward vectors.
-    """
-    return policy @ returns
+from scipy.optimize import minimize
 
 
-def calc_ser(self, policy, returns):
+def objective(strategy, expected_returns, u):
     """
-    This method calculates the scalarised expected returns for a policy and returns.
-    :param policy: An action distribution.
-    :param returns: A array of reward vectors.
-    :return: The SER.
+    The objective function to minimise for is the negative SER, as we want to maximise for the SER.
+    :param strategy: The current estimate for the best response strategy.
+    :param expected_returns: The expected returns given all other players' strategies.
+    :param u: The utility function of this agent.
+    :return: A best response policy.
     """
-    expected_vec = calc_expected_vec(policy, returns)
-    ser = u(expected_vec)
-    return ser
-
-
-def objective(strategy):
-    """
-    The objective to minimize for.
-    :param policy: The current guess for optimal policy.
-    :return: The negative SER from this policy because we want to minimize it.
-    """
-    returns = q_table[state]
-    return - calc_ser(policy, returns)
-'''
+    expected_vec = strategy @ expected_returns  # The expected vector of the strategy applied to the expected returns.
+    objective = - u(expected_vec)  # The negative utility.
+    return objective
 
 
 def best_response(u, player, payoff_matrix, joint_strategy):
     """
-    This function calculates a best response given a payoff matrix and the opponent strategies.
-    :param u: The utility function.
-    :param payoff_matrix: The payoff matrix.
-    :param op_strategies: The opponent strategies. This looks as follows [[p1,1, .., p1,k], ..., [pi,1, ..., pi,l]]
+    This function calculates a best response for a given player.
+    :param u: The utility function for this player.
+    :param player: The player id.
+    :param payoff_matrix: The payoff matrix for this player.
+    :param joint_strategy: The joint strategy of all players.
     :return: A best response strategy.
     """
     num_objectives = payoff_matrix.shape[-1]
@@ -66,9 +47,14 @@ def best_response(u, player, payoff_matrix, joint_strategy):
         # just reduced to one action resulting in the expected return now.
         expected_returns = np.sum(expected_returns, axis=opponent, keepdims=True)
 
-    expected_returns = expected_returns.reshape(num_actions, num_objectives)
-    print(expected_returns)
-    best_response = np.zeros(3)
+    expected_returns = expected_returns.reshape(num_actions, num_objectives)  # Cast the result to a correct shape.
+
+    unif_strategy = np.full(num_actions, 1 / num_actions)  # A uniform strategy as first guess for the optimiser.
+    bounds = [(0, 1)] * num_actions  # Constrain probabilities to 0 and 1.
+    constraints = {'type': 'eq', 'fun': lambda x: np.sum(x) - 1}  # Equality constraint is equal to zero by default.
+    res = minimize(lambda x: objective(x, expected_returns, u), unif_strategy, bounds=bounds, constraints=constraints)
+    best_response = res['x'] / np.sum(res['x'])  # In case of floating point errors.
+
     return best_response
 
 
@@ -78,4 +64,5 @@ if __name__ == '__main__':
     player_actions = monfg[0].shape[:-1]  # Get the number of actions available to each player.
     player = 1
     strategy = [np.array([0.5, 0.3, 0.2]), np.array([0.2, 0.8]), np.array([1., 0., 0.])]  # Necessary for NumPy.
-    best_response(u, player, monfg[player], strategy)
+    br = best_response(u, player, monfg[player], strategy)
+    print(br)
